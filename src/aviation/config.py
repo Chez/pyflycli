9c214e8318675ca500b9581381b4892bcd144eb2
k-zehnder <kjzehnder3@gmail.com>
2022-02-17 15:10:48 -0800

    control flow

--- src/aviation/config.py
+++ src/aviation/config.py
@@ -6,6 +6,15 @@ from pathlib import Path
 from aviation.database import AsyncDatabaseHandler, DummyAsyncDatabaseHandler
 from aviation.errors import *
 
+class Log:
+    def good(self):
+        message = "[INFO] Database status: "
+        status = typer.style("good", fg=typer.colors.GREEN, bold=True)
+        
+    def success_local_db(self):
+        message = "[INFO] Database status: "
+        status = typer.style("no postgres. loaded local sqlite db", fg=typer.colors.GREEN, bold=True)
+        typer.echo(message + status)
 
 def init_app() -> int:
     """Initialize the application."""
@@ -15,27 +24,26 @@ def init_app() -> int:
     return SUCCESS
 
 def _init_database() -> int:
+    log = Log()
     asdb = AsyncDatabaseHandler()
     try:
         message = "[INFO] Database status: "
-        # db_init_error = DB_READ_ERROR
-        db_init_error = asdb.run("is_awake") # DB_READ_ERROR to simulate failed PG load.
+        db_init_error = DB_READ_ERROR
+        # db_init_error = asdb.run("is_awake") # DB_READ_ERROR to simulate failed PG load.
         if db_init_error:
             try:
-                status = typer.style("[INFO] PG DB failed.", fg=typer.colors.RED, bold=True)
+                status = typer.style("postgres db failed.", fg=typer.colors.RED, bold=True)
                 typer.echo(message + status)
 
                 dh = DummyAsyncDatabaseHandler()
                 dh.run("parse_data")
                 
-                status = typer.style("[INFO] Success local DB.", fg=typer.colors.GREEN, bold=True)
-                typer.echo(message + status)
+                log.success_local_db()
             except:
                 status = typer.style("bad", fg=typer.colors.RED, bold=True)
                 typer.echo(message + status)        
                 raise typer.Exit(1)
-        status = typer.style("good", fg=typer.colors.GREEN, bold=True)
-        typer.echo(message + status)
+        log.good()
     except OSError:
         return DB_READ_ERROR
     return SUCCESS
